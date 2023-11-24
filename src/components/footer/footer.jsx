4c4cb1676d32bf73714b8633367f36c9bb090647
Mamelq <p.mamelka99@wp.pl>
2023-11-24 14:26:20 +0100

    opinion to footer

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -3,6 +3,8 @@ import './Footer.scss';
 
 const Footer = () => {
     const [isOpenText, setIsOpenText] = useState(true);
+    const [ratings, setRatings] = useState([]);
+    const [newRating, setNewRating] = useState(0);
 
     useEffect(() => {
         const interval = setInterval(() => {
@@ -11,6 +13,28 @@ const Footer = () => {
 
         return () => clearInterval(interval);
     }, []);
+
+    const handleRatingChange = (event) => {
+        const value = parseInt(event.target.value, 10);
+        setNewRating(value);
+    };
+
+    const handleAddRating = () => {
+        if (newRating >= 1 && newRating <= 5) {
+            setRatings([...ratings, newRating]);
+            setNewRating(0);
+        } else {
+            alert('Ocena musi być w skali od 1 do 5.');
+        }
+    };
+
+    const calculateAverageRating = () => {
+        if (ratings.length === 0) {
+            return 0;
+        }
+        const sum = ratings.reduce((acc, rating) => acc + rating, 0);
+        return sum / ratings.length;
+    };
 
     return (
         <footer className="footer">
@@ -22,7 +46,21 @@ const Footer = () => {
                 </div>
                 <div className="footer__column">
                     <h3>Oceny</h3>
-                    <p>Oceny: 4.8 / 5</p>
+                    <p>Oceny: {calculateAverageRating().toFixed(2)} / 5</p>
+                    <div>
+                        <label>
+                            Twoja ocena:
+                            <select value={newRating} onChange={handleRatingChange}>
+                                <option value="0">Wybierz</option>
+                                <option value="1">1 gwiazdka</option>
+                                <option value="2">2 gwiazdki</option>
+                                <option value="3">3 gwiazdki</option>
+                                <option value="4">4 gwiazdki</option>
+                                <option value="5">5 gwiazdek</option>
+                            </select>
+                        </label>
+                        <button onClick={handleAddRating}>Dodaj ocenę</button>
+                    </div>
                 </div>
                 <div className="footer__column">
                     <h3>Kontakt</h3>
